d75639bee0bfa21609f6560c2e4bf220edb95f10
810git <andyvonde66beretta@gmail.com>
2020-11-18 08:07:01 +0900

    change esp32 voltage

--- esp32/sketch_nov_smell/sketch_nov_smell.ino
+++ esp32/sketch_nov_smell/sketch_nov_smell.ino
@@ -1,53 +1,41 @@
 #define PIN_HEATER 14 // D14(A0)
 #define PIN_SENSOR 15 // D15(A1)
-#define PIN_OUTPUT 34 // A3
+#define PIN_OUTPUT 34 // A3 for esp32
 void setup() {
  pinMode(PIN_HEATER,OUTPUT);
  pinMode(PIN_SENSOR,OUTPUT);
+ 
  digitalWrite(PIN_HEATER,HIGH); // Heater Off
  digitalWrite(PIN_SENSOR,LOW); // Sensor Pullup Off
 
  Serial.begin(9600);
 }
 void loop() {
- int val=0;
- float valflo=0;
- float vout=0;
- float resultflo=0;
+  int val=0;
+  //float val=0; not good ?
+  float voutka=0;
+  float rs=0;
+  float rl=27;// OK?
 
- delay(237);
- digitalWrite(PIN_SENSOR,HIGH); // Sensor Pullup On
- delay(3);
- val = analogRead(PIN_OUTPUT); // Get Sensor Voltage
- valflo = analogRead(PIN_OUTPUT); // Get Sensor Voltage
- delay(2);
- digitalWrite(PIN_SENSOR,LOW); // Sensor Pullup Off
-
- digitalWrite(PIN_HEATER,LOW); // Heater On
- delay(8);
- digitalWrite(PIN_HEATER,HIGH); // Heater Of
-
- //Serial.println("---------------------");
- //Serial.println(val);
+  delay(237);
+  digitalWrite(PIN_SENSOR,HIGH); // Sensor Pullup On 
+  delay(3);
+  val = analogRead(PIN_OUTPUT); // Get Sensor Voltage 
+  voutka = 3.3 - val * 3.3 /4096; // Vout ? for esp32 0-4096max / 0-3.3Voltage
+  delay(2);
+  digitalWrite(PIN_SENSOR,LOW); // Sensor Pullup Off
+  
+  digitalWrite(PIN_HEATER,LOW); // Heater On 
+  delay(8);
+  digitalWrite(PIN_HEATER,HIGH); // Heater Off
  
- Serial.println("maybe..voltage");
- Serial.println(valflo/4096*5);
- //Serial.print((5 - valflo/4096*5) * 1000 /valflo/4096*5);
- //not good ? vout = valflo/4096*5;
- vout = 5 - valflo/4096*5;
- 
- resultflo = (5 - vout)/vout * 1000 / (56*1000 *   70);
- 
- //Serial.print(resultflo , 8);
- //Serial.println("   QQQQ");
- //Serial.println(resultflo , 8);
 
- float valfix=100000;
+  //Serial.println(val,8);
+  //Serial.println(voutka,8);
+  rs = (3.3 - voutka) / voutka * rl;
+  Serial.println(rs,8);
 
- //Serial.print(resultflo, 8);
- Serial.println("   /R0 desu");
- Serial.println(resultflo , 8);
- 
- //Serial.println("---------------------");
+  //R0 = 7.05 ohm ? air ? 70.5 ohm for ethanol?
+  Serial.println(rs/70.5,8);
 
 }
