c3245fb263a5985b1f709894b44a55d4d31df950
810git <andyvonde66beretta@gmail.com>
2020-11-21 21:10:21 +0900

    Nov 21

--- esp32/sketch_nov_smell/sketch_nov_smell.ino
+++ esp32/sketch_nov_smell/sketch_nov_smell.ino
@@ -11,17 +11,14 @@ void setup() {
  Serial.begin(9600);
 }
 void loop() {
+  // only digitalread...
   int val=0;
-  //float val=0; not good ?
-  float voutka=0;
-  float rs=0;
-  float rl=27;// OK?
 
   delay(237);
   digitalWrite(PIN_SENSOR,HIGH); // Sensor Pullup On 
   delay(3);
   val = analogRead(PIN_OUTPUT); // Get Sensor Voltage 
-  voutka = 3.3 - val * 3.3 /4096; // Vout ? for esp32 0-4096max / 0-3.3Voltage
+
   delay(2);
   digitalWrite(PIN_SENSOR,LOW); // Sensor Pullup Off
   
@@ -30,12 +27,6 @@ void loop() {
   digitalWrite(PIN_HEATER,HIGH); // Heater Off
  
 
-  //Serial.println(val,8);
-  //Serial.println(voutka,8);
-  rs = (3.3 - voutka) / voutka * rl;
-  Serial.println(rs,8);
-
-  //R0 = 7.05 ohm ? air ? 70.5 ohm for ethanol?
-  Serial.println(rs/70.5,8);
+ Serial.println(val); 
 
 }
